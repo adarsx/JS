b59677430fe658f1fcdbe600d21a4772d5222f42
adarsx <118877297+adarsx@users.noreply.github.com>
2025-04-30 18:04:03 +0000

    variables

---
+++ 01_basic/01_valriables.js
@@ -0,0 +1,10 @@
+const accountId = 1445
+let  accountEmail = "adarsh@gmail.com"
+var accountPass = "1234"
+accountCity = "delhi"
+let accountState
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPass,accountState])
