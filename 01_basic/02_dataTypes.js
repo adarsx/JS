28cf235acb7fcfc9456b9e70f6cabede88cc1778
adarsx <118877297+adarsx@users.noreply.github.com>
2025-04-30 18:00:42 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) -> we are using node js, not browser
+
+
+let name = "adarsh"
+let num = 30
+let isright = true
+let state;
+
+console.log( typeof undefined)
