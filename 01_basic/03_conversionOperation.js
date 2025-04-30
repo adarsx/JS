acd9cfce8cabad98b3d46b3bcdbb7eaa03ed8e7f
adarsx <118877297+adarsx@users.noreply.github.com>
2025-04-30 18:17:01 +0000

    conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "true"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let value = Number(score)
+console.log(typeof value);
+console.log(value);
+
+
+// "33" => 33
+// "33ad" => NaN
+
+let islogged = 0
+
+let islogbool = Boolean(islogged)
+
+console.log(islogbool);
+
+
+// *************operation******************
+
+let n = 3
+let neg = -n
+
+console.log(neg)
+console.log(n)
+
+console.log("1" + 2)
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
